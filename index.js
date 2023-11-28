86f7ebcdc3fddeb740526db460f5d7f7ae316113
siopipin <siojurnalispipin@hotmail.com>
2023-11-28 12:06:09 +0700

    indexjs

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // multer untuk upload file
-const storage = multer()
+const formdata = multer();
 
 // define route
 app.get("/", (req, res) => {
@@ -31,7 +31,8 @@ app.get("/users", (req, res) => {
   });
 });
 
-app.post("/users", (req, res) => {  // kirim data dari json body.
+app.post("/users", (req, res) => {
+  // kirim data dari json body.
   connection.query("INSERT INTO tbl_users SET ?", req.body, (err, result) => {
     if (err) {
       console.log(err);
@@ -39,12 +40,88 @@ app.post("/users", (req, res) => {  // kirim data dari json body.
     } else {
       res.send(result);
     }
+  });
+});
+
+app.post("/users-formdata", formdata.none(), (req, res) => {
+  //storage.none() digunakan untuk terima form-data multipart
+  res.send(req.body);
+});
+
+// tambahkan data customer
+app.get("/customers", (req, res) => {
+  connection.query("select * from tbl_users", (err, result) => {
+    if(err) {
+      res.status(400).send(err);
+    } else {
+      res.json({data: result})
+    }
+  })
+});
+
+app.get("/customers/:id", (req, res) => {
+  const id = req.params.id
+
+  connection.query(`SELECT * FROM tbl_users WHERE id = ?`, [id], (err, result) => {
+    if(err) {
+      res.status(400).send("err")
+    } else {
+      if(result.length <= 0) {
+      res.status(404).send("tidak ada data")
+      } else {
+        res.json({data: result})
+      }
+    }
   })
 })
 
-app.post("/users-formdata", storage.none(), (req, res) => {  //storage.none() digunakan untuk terima form-data multipart
-  res.send(req.body);
+app.post("/customers", (req, res) => {
+  
+  const data = req.body;
+  // connection.query("INSERT INTO tbl_users SET ?", [data], (err, result) => {
+  //   if(err) {
+  //     res.status(400).send(err)
+  //   } else {
+  //     if(result.affectedRows === 1) {
+  //       res.json({msg: "Success"})
+  //     } else {
+  //       res.json({msg: "not success"})
+  //     }
+  //   }
+  // })
+
+  const name = req.body.name1
+
+  const query = `INSERT INTO Customers (CustomerName, ContactName, Address City, PostalCode, Country) VALUES (?, ?, ?, ?,?,?)`
+
+  connection.query(query, [name,b,c,d,e,f], (err, result) => {
+    if(err) {
+      res.status(400).send(err)
+    } else {
+      if(result.affectedRows === 1) {
+        res.json({msg: "Success"})
+      } else {
+        res.json({msg: "not success"})
+      }
+    }
+  })
+
+
 })
+
+// tambahkan data customer
+app.post("/product", formdata.none(), (req, res) => {
+  const kumpulandata = req.body;
+  //tampilkan data user dengan id = 1
+  res.json({ data: kumpulandata });
+});
+
+// route query
+
+app.get("/users/detail", (req, res) => {
+  const jk = req.query.jk;
+  const hobby = req.query.hobby;
+});
 
 // jalankan server
 app.listen(port, () => {
