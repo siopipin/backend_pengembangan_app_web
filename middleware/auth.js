cee5ea1f3a5de0a687282eecf113389197d41427
siopipin <siojurnalispipin@hotmail.com>
2024-01-10 16:02:39 +0700

    update auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization'];
   if (token == null) return res.sendStatus(401); // Unauthorized
 
-  jwt.verify(token, 'irvan098867', (err, user) => {
+  jwt.verify(token, 'sio12345', (err, user) => {
     if (err) return res.sendStatus(403); // Forbidden
     req.user = user;
     next();
